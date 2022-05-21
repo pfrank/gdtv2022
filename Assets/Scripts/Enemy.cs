27ae867f138eb58887d8951d7b043fa198a23b36
Paul Frank <phfrank@protonmail.com>
2022-05-21 12:07:02 -0400

    Change enemy pathfinding from NavMesh to Waypoints

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,35 +1,74 @@
 using UnityEngine;
-using UnityEngine.AI;
+using System.Collections.Generic;
 
 public class Enemy : MonoBehaviour
 {
-    [SerializeField] int hitPoints = 10;
+    [SerializeField] int health = 10;
+    [SerializeField] int damage = 10;
+    [SerializeField] float speed = 10f;
 
-    private NavMeshAgent navMeshAgent;
-    private GameObject target;
+    private Path path;
+    private int currWaypointIndex = 0;
+    private Transform target;
+
     private void Start()
     {
-        navMeshAgent = GetComponent<NavMeshAgent>();
-        target = GameObject.FindGameObjectWithTag("Target");
-        navMeshAgent.destination = target.transform.position;
+        path = GameObject.Find("Path").GetComponent<Path>();
+        target = GameObject.Find("Target").transform;
+
+        // The target is our final waypoint
+        path.waypoints.Add(target);
     }
 
     private void Update()
     {
-        if (TargetReached())
-        {
-            Debug.Log("Deal Damage to Castle");
-            Destroy(gameObject);
-        }
-        else if (hitPoints <= 0f)
-        {
-            Debug.Log("Enemy Destroyed!");
-            Destroy(gameObject);
-        } 
+        MoveAlongPath();
     }
 
-    private bool TargetReached()
+    private void MoveAlongPath()
     {
-        return ((transform.position - target.transform.position).magnitude < 0.1f);
+        // Has the target been reached?
+        if (transform.position == target.position)
+        {
+            TargetReached();
+            return;
+        }
+
+        if (currWaypointIndex >= path.waypoints.Count)
+            return;
+
+        Vector3 targetPosition = path.waypoints[currWaypointIndex].position;
+        transform.position = Vector3.MoveTowards(
+            transform.position,
+            targetPosition,
+            speed * Time.deltaTime
+            );
+
+        transform.LookAt(targetPosition);
+
+        // Waypoint reached, go to the next waypoint
+        if (transform.position == targetPosition)
+            currWaypointIndex++;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Debug.Log("Enemy Destroyed!");
+        Destroy(gameObject);
+    }
+
+    private void TargetReached()
+    {
+        Debug.Log("Target Reached!");
+        Destroy(gameObject);
     }
 }
